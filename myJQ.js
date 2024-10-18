fab4cc20c5e1eb13b408544037210d78f4024996
Priyanshu <priyanshurazz4@gmail.com>
2024-10-18 18:27:05 +0530

    first commit

---
+++ myJQ.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('p').click(function () {
+      console.log("Clicked!");
+    //   $(this).hide(3000,()=>{
+    //     console.log("Hidden");
+    //     $(this).show(3000);
+    //   });
+    // $(this).fadeOut(1000);
+    });
+  });
